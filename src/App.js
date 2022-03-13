0c937bc22fc5cf89aed1b9722b16ff014a70ac93
GiftBanda <bandagift42@gmail.com>
2022-03-13 22:15:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
+
+  //login url
+  const url = 'http://localhost:5000/api/auth/login';
+
+  //hold the user data
+  const handleChange = (setter) => (e) => {
+    setter(e.currentTarget.value);
+    
+  }
+
+  const handleSubmit = async (e) => {
+    //prevent the default behavior of the form
+    e.preventDefault()
+    try {
+
+      const user = { email, password }
+
+      const { data } = await axios.post(url, user)
+
+      //set the user state
+      setUser(data)
+
+      //set user to local storage
+      localStorage.setItem('user', JSON.stringify(data))
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  //check if the user is logged in 
+  if(user) {
+    return <h1>User is logged in</h1>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label for='email'>Email</label>
+        <input type='email' autoComplete='false' onChange={handleChange(setEmail) } />
+        <label for='password'>Password</label>
+        <input type='password' onChange={handleChange(setPassword)} />
+        <button type='submit'>Login</button>
+      </form>
     </div>
   );
 }
